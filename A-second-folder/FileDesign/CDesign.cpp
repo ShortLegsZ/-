d64681ceec431443b2a85396ecd3b65699fddef2
ShortLegsZ <63696539+ShortLegsZ@users.noreply.github.com>
2020-07-19 22:32:37 +0800

    Add files via upload
    upload a few floder

---
+++ A-second-folder/FileDesign/CDesign.cpp
@@ -0,0 +1,34 @@
+﻿// CDesign.cpp: 实现文件
+//
+
+#include "pch.h"
+#include "FileDesign.h"
+#include "CDesign.h"
+#include "afxdialogex.h"
+
+
+// CDesign 对话框
+
+IMPLEMENT_DYNAMIC(CDesign, CDialogEx)
+
+CDesign::CDesign(CWnd* pParent /*=nullptr*/)
+	: CDialogEx(IDD_DIALOG1, pParent)
+{
+
+}
+
+CDesign::~CDesign()
+{
+}
+
+void CDesign::DoDataExchange(CDataExchange* pDX)
+{
+	CDialogEx::DoDataExchange(pDX);
+}
+
+
+BEGIN_MESSAGE_MAP(CDesign, CDialogEx)
+END_MESSAGE_MAP()
+
+
+// CDesign 消息处理程序
---
+++ A-second-folder/FileDesign/CDesign.h
@@ -0,0 +1,23 @@
+﻿#pragma once
+
+
+// CDesign 对话框
+
+class CDesign : public CDialogEx
+{
+	DECLARE_DYNAMIC(CDesign)
+
+public:
+	CDesign(CWnd* pParent = nullptr);   // 标准构造函数
+	virtual ~CDesign();
+
+// 对话框数据
+#ifdef AFX_DESIGN_TIME
+	enum { IDD = IDD_DIALOG1 };
+#endif
+
+protected:
+	virtual void DoDataExchange(CDataExchange* pDX);    // DDX/DDV 支持
+
+	DECLARE_MESSAGE_MAP()
+};
